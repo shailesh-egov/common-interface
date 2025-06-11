50a3db85534cb476cc79437782aaaf47aa51c3ba
Shailesh Pandey <110380977+shailesh-egov@users.noreply.github.com>
2025-06-11 14:20:20 +0530

    Updated library

--- data.go
+++ data.go
@@ -3,15 +3,18 @@ package common
 import "errors"
 
 type CommonData struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	TenantID string `json:"tenantId"`
 }
 
 // Implements Entity interface
-func (c *CommonData) GetID() string       { return c.ID }
-func (c *CommonData) SetID(id string)     { c.ID = id }
-func (c *CommonData) GetName() string     { return c.Name }
-func (c *CommonData) SetName(name string) { c.Name = name }
+func (c *CommonData) GetID() string         { return c.ID }
+func (c *CommonData) SetID(id string)       { c.ID = id }
+func (c *CommonData) GetName() string       { return c.Name }
+func (c *CommonData) SetName(name string)   { c.Name = name }
+func (c *CommonData) GetTenantID() string   { return c.TenantID }
+func (c *CommonData) SetTenantID(id string) { c.TenantID = id }
 
 // Implements Validator interface
 func (c *CommonData) Validate() error {
@@ -20,6 +23,9 @@ func (c *CommonData) Validate() error {
 	}
 	if c.Name == "" {
 		return errors.New("name is required")
+	}
+	if c.TenantID == "" {
+		return errors.New("tenantId is required")
 	}
 	return nil
 }
